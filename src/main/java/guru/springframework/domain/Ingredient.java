42a92a76b525d9ffbb0afb52fbb435218244f073
Matthieu Rambaud <rambaud.matthieu@gmail.com>
2018-09-26 09:48:28 +0200

    switch project from SQL database to noSQL

--- src/main/java/guru/springframework/domain/Ingredient.java
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -5,6 +5,8 @@ import lombok.Setter;
 
 import java.math.BigDecimal;
 
+import org.springframework.data.mongodb.core.mapping.DBRef;
+
 /**
  * Created by jt on 6/13/17.
  */
@@ -16,8 +18,8 @@ public class Ingredient {
     private String description;
     private BigDecimal amount;
 
+    @DBRef
     private UnitOfMeasure uom;
-    private Recipe recipe;
 
     public Ingredient() {
     }
@@ -32,7 +34,7 @@ public class Ingredient {
         this.description = description;
         this.amount = amount;
         this.uom = uom;
-        this.recipe = recipe;
+        //this.recipe = recipe;
     }
 
 }
